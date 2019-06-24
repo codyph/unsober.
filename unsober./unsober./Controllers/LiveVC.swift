ec7852c52bef06bcb4dff65ce0110b2560ed1a96
Cody Philipp <codyphilipp@gmail.com>
2019-06-24 20:34:45 +0800

    figured out endSession button issue with the view not allowing userinteraction

--- unsober./unsober./Controllers/LiveVC.swift
+++ unsober./unsober./Controllers/LiveVC.swift
@@ -13,35 +13,51 @@ class LiveVC: UIViewController {
     // Variables
     
     
+    
     // Outlets
-    @IBOutlet var fullView: UIView!
+    @IBOutlet weak var fullView: UIView!
+    @IBOutlet weak var childView: UIView!
+    
     @IBOutlet weak var endSessionViewHeight: NSLayoutConstraint!
     @IBOutlet weak var endSessionBotConstraint: NSLayoutConstraint!
+    @IBOutlet weak var fullViewBotConstraint: NSLayoutConstraint!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //endSessionViewHeight.constant = view.frame.height/5
-        endSessionBotConstraint.constant = endSessionViewHeight.constant
         
+        fullViewBotConstraint.constant += endSessionViewHeight.constant
+        endSessionBotConstraint.constant = -endSessionViewHeight.constant
     }
     
     
     @IBAction func didSwipe(_ sender: UISwipeGestureRecognizer) {
         let duration = 0.4
         UIView.animate(withDuration: duration) {
-            self.moveViews(sender: sender, view: self.fullView)
+            self.moveViews(sender: sender, fullView: self.fullView, childView: self.childView)
         }
     }
     
-    func moveViews(sender: UISwipeGestureRecognizer, view: UIView){
+    
+    func moveViews(sender: UISwipeGestureRecognizer, fullView: UIView, childView: UIView) {
         if sender.direction == UISwipeGestureRecognizer.Direction.up {
-            view.center.y -= endSessionViewHeight.constant
+            fullView.center.y -= endSessionViewHeight.constant
+            childView.center.y -= endSessionViewHeight.constant
         }
         if sender.direction == UISwipeGestureRecognizer.Direction.down {
-            view.center.y += endSessionViewHeight.constant
+            fullView.center.y += endSessionViewHeight.constant
+            childView.center.y += endSessionViewHeight.constant
         }
     }
+    
+    @IBAction func endSessionBtnTapped(_ sender: Any) {
+        
+        // CHANGE THIS BECAUSE YOURE TAKING UP SO MUCH MEMORY
+        // CHANGE AS IN : FIGURE OUT HOW TO GO BACK VIEW CONTROLLERS - STOP
+        // PUSHING TO ANOTHER VIEW CONTROLLLER. YOU ESSENTIALLY NEVER CLOSE ANY
+        // VIEW CONTROLLER AND ITS BAD!!!!
+        
+    }
+    
 
 }
