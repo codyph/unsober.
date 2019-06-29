f5e9bfe4246ff2e2129b3809b8017c64726bf90a
Cody Philipp <codyphilipp@gmail.com>
2019-06-29 23:15:29 +0800

    added buttons, BAC UIView, and a little tester to bring the app more to life

--- unsober./unsober./Controllers/LiveVC.swift
+++ unsober./unsober./Controllers/LiveVC.swift
@@ -11,12 +11,20 @@ import UIKit
 class LiveVC: UIViewController {
 
     // Variables
+    var swipeMoved: Int = 0
     
+    var bacNumber: Double = 0.00
+    var bacNumberArrayTest = [0.021,0.0157,0.01039,0.031,0.0034,0.0057] //Tester
     
+    // ** NEED TO CODE THAT YOU CAN ONLY SWIPE UPWARDS AND DOWNWARDS ONCE
     
     // Outlets
     @IBOutlet weak var fullView: UIView!
     @IBOutlet weak var childView: UIView!
+    @IBOutlet weak var bacView: UIView!
+    
+    @IBOutlet weak var bacTextField: UITextField!
+    @IBOutlet weak var timeTextField: UITextField!
     
     @IBOutlet weak var endSessionViewHeight: NSLayoutConstraint!
     @IBOutlet weak var endSessionBotConstraint: NSLayoutConstraint!
@@ -25,9 +33,11 @@ class LiveVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        bacView.layer.cornerRadius = 20
         fullViewBotConstraint.constant += endSessionViewHeight.constant
         endSessionBotConstraint.constant = -endSessionViewHeight.constant
+        
+        soberCheck()
     }
     
     
@@ -40,24 +50,80 @@ class LiveVC: UIViewController {
     
     
     func moveViews(sender: UISwipeGestureRecognizer, fullView: UIView, childView: UIView) {
-        if sender.direction == UISwipeGestureRecognizer.Direction.up {
+        if sender.direction == UISwipeGestureRecognizer.Direction.up && swipeMoved == 0 {
             fullView.center.y -= endSessionViewHeight.constant
             childView.center.y -= endSessionViewHeight.constant
+            swipeMoved = 1
         }
-        if sender.direction == UISwipeGestureRecognizer.Direction.down {
+        if sender.direction == UISwipeGestureRecognizer.Direction.down && swipeMoved == 1{
             fullView.center.y += endSessionViewHeight.constant
             childView.center.y += endSessionViewHeight.constant
+            swipeMoved = 0
         }
     }
     
     @IBAction func endSessionBtnTapped(_ sender: Any) {
-        
-        // CHANGE THIS BECAUSE YOURE TAKING UP SO MUCH MEMORY
-        // CHANGE AS IN : FIGURE OUT HOW TO GO BACK VIEW CONTROLLERS - STOP
-        // PUSHING TO ANOTHER VIEW CONTROLLLER. YOU ESSENTIALLY NEVER CLOSE ANY
-        // VIEW CONTROLLER AND ITS BAD!!!!
-        
+        dismiss(animated: true, completion: nil)
     }
     
-
+    
+    func soberCheck() {
+        if bacNumber > 0.00 {
+            bacTextField.text = String(format: "%.4f", bacNumber)
+            timeTextField.text = calculateTimeUntilSober(bacNum: bacNumber)
+        } else {
+            bacTextField.text = "You're Sober!" // Should I include a gallery of "sayings" that could be put here? As in, every time you visit the page, there's a different greeting //
+            timeTextField.text = "Time to start drinking?" // Maybe also include above idea here too???
+        }
+    }
+    
+    func calculateBac(){
+        /*
+         eBAC = (c/2 - GC/weight) - (Beta*t)
+        */
+    }
+    
+    func calculateTimeUntilSober(bacNum: Double) -> String {
+        var timeStatement = [String]()
+        
+        let timeTest = bacNum*100
+        let timeHour = String(format: "%.0f", timeTest)
+        
+        let timeMinuteDecimal = timeTest - floor(timeTest)
+        let timeMinuteN = timeMinuteDecimal*60
+        let timeMinute = String(format: "%.0f", timeMinuteN)
+        
+        let timeSecondDecimal = timeMinuteN - floor(timeMinuteN)
+        let timeSecondN = timeSecondDecimal*60
+        let timeSecond = String(format: "%.0f", timeSecondN)
+        
+        if timeHour != "0" {
+            timeStatement.append(timeHour+" hours")
+        }
+        if timeMinute != "0" {
+            timeStatement.append(timeMinute+" mins")
+        }
+        if timeSecond != "0" {
+            timeStatement.append(timeSecond+" secs")
+        }
+        
+        return timeStatement.joined(separator: ", ")
+    }
+    
+    
+    
+    @IBAction func greenAddBtnTappedTest(_ sender: Any) {
+        bacNumber += bacNumberArrayTest.max()!
+        soberCheck()
+    }
+    @IBAction func greenAddBtnTapped2(_ sender: Any) {
+        bacNumber += bacNumberArrayTest.min()!
+        soberCheck()
+    }
+    @IBAction func greenAddBtnTapped3(_ sender: Any) {
+        bacNumber += bacNumberArrayTest.max()! - 3*bacNumberArrayTest.min()!
+        soberCheck()
+    }
+    
+    
 }
